6a8fb7e77cdb3fcef8dab8b3b5724d49dbd89562
Sanjeev Beekeeper PC office <sanjeevbeekeeper@gmail.com>
2017-01-07 12:16:32 +0530

    Updated the 'talk-to-us' with a proper url.

--- banner-talktous.php
+++ banner-talktous.php
@@ -1,26 +1,21 @@
 <!-- home -->
 <?php if(is_page(6)) { ?>
-    <a href="<?php echo get_page_link(14); ?>"> Talk to Us</a>
+    <!-- <a href="<?php echo get_page_link(14); ?>"> Talk to Us</a> -->
     <section class="banner-container primary-color text-center">
         <h3 class="banner-text">
             We can provide a Complete Website solution? ss
         </h3>
-        <button type="button" class="btn btn-secondary btn-lg">
-            <a href="<?php echo get_page_link(14); ?>"> Talk to Us</a>
-        </button>
+        <a class="btn btn-secondary btn-lg" href="<?php echo get_page_link(14); ?>">Talk to Us</a>
     </section>
 <?php } ?>
 
 <!-- Responsive Webdesign -->
 <?php if(is_page(33)) { ?>
-    <a href="<?php echo get_page_link(14); ?>"> Talk to Us Responsive</a>
     <section class="banner-container primary-color text-center">
         <h3 class="banner-text">
             Looking for a Visually Stunning Responsive Website?
         </h3>
-        <button type="button" name="button" class="btn btn-secondary btn-lg">
-            <a href="<?php get_permalink( get_page_by_path( 'talk-to-us' ) ); ?>"> Talk to Us</a>
-        </button>
+        <a class="btn btn-secondary btn-lg" href="<?php echo get_page_link(14); ?>">Talk to Us</a>
     </section>
 <?php } ?>
 
@@ -30,9 +25,7 @@
         <h3 class="banner-text">
             Yes, We also Build Responsive WordPress sites
         </h3>
-        <button type="button" name="button" class="btn btn-secondary btn-lg">
-            <a href="<?php get_permalink( get_page_by_path( 'talk-to-us' ) ); ?>"> Talk to Us</a>
-        </button>
+        <a class="btn btn-secondary btn-lg" href="<?php echo get_page_link(14); ?>">Talk to Us</a>
     </section>
 <?php } ?>
 
@@ -42,9 +35,7 @@
         <h3 class="banner-text">
             We provide Backup, Update, and New Features with an affordable cost.
         </h3>
-        <button type="button" name="button" class="btn btn-secondary btn-lg">
-            <a href="<?php get_permalink( get_page_by_path( 'talk-to-us' ) ); ?>"> Talk to Us</a>
-        </button>
+        <a class="btn btn-secondary btn-lg" href="<?php echo get_page_link(14); ?>">Talk to Us</a>
     </section>
 <?php } ?>
 
@@ -54,9 +45,7 @@
         <h3 class="banner-text">
             We can Setup a Creative Blog with User-Friendly CMS.
         </h3>
-        <button type="button" name="button" class="btn btn-secondary btn-lg">
-            <a href="<?php get_permalink( get_page_by_path( 'talk-to-us' ) ); ?>"> Talk to Us</a>
-        </button>
+        <a class="btn btn-secondary btn-lg" href="<?php echo get_page_link(14); ?>">Talk to Us</a>
     </section>
 <?php } ?>
 
@@ -66,8 +55,6 @@
         <h3 class="banner-text">
             We are Nice People!
         </h3>
-        <button type="button" name="button" class="btn btn-secondary btn-lg">
-            <a href="<?php get_permalink( get_page_by_path( 'talk-to-us' ) ); ?>"> Talk to Us</a>
-        </button>
+        <a class="btn btn-secondary btn-lg" href="<?php echo get_page_link(14); ?>">Talk to Us</a>
     </section>
 <?php } ?>
