d209bd1a1e76d66f23756f2dd5353c262616c951
Sanjeev Beekeeper PC office <sanjeevbeekeeper@gmail.com>
2017-01-09 19:04:22 +0530

    Newly added.

---
+++ post-pagination.php
@@ -0,0 +1,13 @@
+<!-- Pagination -->
+<div class="blog--bottom__navigation">
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 text-center">
+            <div class="col-xs-6 col-xs-offset-0 col-sm-6 col-md-5 col-md-offset-1 col-lg-6 col-lg-offset-0 blog-nav-left">
+                <?php previous_posts_link(); ?>
+            </div>
+            <div class="col-xs-6 col-xs-offset-0 col-sm-6 col-md-5 col-md-offset-0 col-lg-6 col-lg-offset-0 blog-nav-right">
+                <?php next_posts_link(); ?>
+            </div> <!-- // blog-bottom_navigation -->
+        </div> <!-- // column closing -->
+    </div> <!-- // row closing -->
+</div> <!-- // blog-bottom__navigation -->
