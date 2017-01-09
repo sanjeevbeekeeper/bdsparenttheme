c79699169f1afb7580510a79a92454a929c9938d
Sanjeev Beekeeper PC office <sanjeevbeekeeper@gmail.com>
2017-01-09 19:14:19 +0530

    Removed the loop and added all the pages at the top.

--- content.php
+++ content.php
@@ -1,52 +1,47 @@
 <!--
-# This page is used in
+# Used in
 # index.php
+# archive.php
+# search.php
+# single.php
 -->
+<article class="blog--article">
+    <!-- title -->
+    <h3> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h3>
 
-<?php if (have_posts()) :
-    while (have_posts()) : the_post();
-    ?>
+    <!-- category container -->
+    <div class="blog--article__category">
+        <!-- date -->
+        <time> <?php the_time('d M Y'); ?></time>
 
-    <article class="blog--article">
-        <!-- title -->
-        <h3> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h3>
-
-        <!-- category container -->
-        <div class="blog--article__category">
-            <!-- date -->
-            <time> <?php the_time('d M Y'); ?></time>
-
-            <!-- category -->
-            <?php
-                $cat = get_the_category();
-                $sep = ', ';
-                $output = '';
-                //
-                if ($cat) {
-                    foreach ($cat as $category){
-                        $output .= '<a href=" ' . get_category_link( $category->term_id ) . ' ">' . $category->cat_name . '</a>' . $sep;
-                        }
-                    echo trim($output, $sep);
-                    }
-            ?>
-        </div> <!-- // category container -->
-
-        <!-- Featured Image -->
-        <div class="featuredimage--container">
-            <?php the_post_thumbnail(); ?>
-        </div>
-
-
-        <!-- content -->
+        <!-- category -->
         <?php
-            if ( is_home() || is_archive() || is_search() ) {
-                the_excerpt();
+            $cat = get_the_category();
+            $sep = ', ';
+            $output = '';
+            //
+            if ($cat) {
+                foreach ($cat as $category){
+                    $output .= '<a href=" ' . get_category_link( $category->term_id ) . ' ">' . $category->cat_name . '</a>' . $sep;
+                    }
+                echo trim($output, $sep);
                 }
-            else {
-                the_content();
-                }
-            ?>
-    </article>
+        ?>
+    </div> <!-- // category container -->
 
-<?php endwhile; ?>
-<?php endif; ?>
+    <!-- Featured Image -->
+    <div class="featuredimage--container">
+        <?php the_post_thumbnail(); ?>
+    </div>
+
+
+    <!-- content -->
+    <?php
+        if ( is_home() || is_archive() || is_search() ) {
+            the_excerpt();
+            }
+        else {
+            the_content();
+            }
+        ?>
+</article>
