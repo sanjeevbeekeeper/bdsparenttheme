ac30fb8748ab44cbc4cdffb06fe196e1e6843f6a
Sanjeev Beekeeper PC office <sanjeevbeekeeper@gmail.com>
2017-03-07 17:20:20 +0530

    Updated with code.

--- content.php
+++ content.php
@@ -6,39 +6,18 @@
 # single.php
 -->
 <article class="blog--article">
-    <!-- title -->
-    <h3> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h3>
 
-    <!-- category container -->
-    <div class="blog--article__category">
-        <!-- date -->
-        <time> <?php the_time('d M Y'); ?></time>
-
-        <!-- category -->
-        <?php
-            $cat = get_the_category();
-            $sep = ', ';
-            $output = '';
-            //
-            if ($cat) {
-                foreach ($cat as $category){
-                    $output .= '<a href=" ' . get_category_link( $category->term_id ) . ' ">' . $category->cat_name . '</a>' . $sep;
-                    }
-                echo trim($output, $sep);
-                }
-        ?>
-    </div> <!-- // category container -->
-
-    <!-- Featured Image -->
-    <div class="featuredimage--container">
-        <?php the_post_thumbnail(); ?>
-    </div>
-
+    <a class="blog--article__title-date" href="<?php the_permalink(); ?>">
+        <h3>
+            <div class="truncate"><?php the_title(); ?></div>
+            <time> <?php the_time('d M Y'); ?></time>
+        </h3>
+    </a>
 
     <!-- content -->
     <?php
         if ( is_home() || is_archive() || is_search() ) {
-            the_excerpt();
+            // the_excerpt();
             }
         else {
             the_content();
