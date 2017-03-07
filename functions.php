c470dcccd18db2beb132243a12b2397846267fcb
Sanjeev Beekeeper PC office <sanjeevbeekeeper@gmail.com>
2017-03-07 17:21:12 +0530

    Added search query for blog post only.

--- functions.php
+++ functions.php
@@ -2,17 +2,48 @@
 	// Boostrap Styles and Scripts. 22 dec 2016
 	function bootstrap_style_script() {
 
+		// ===== Deregister WordPress Default jQuery Script and Add custom jQuery in the footer
+		wp_deregister_script( 'jquery' );
+		wp_enqueue_script(
+			'jquery',
+			'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js',
+			array(),
+			'3.1.1',
+			true
+			);
+
 		// bootstrap js
 		wp_enqueue_script(
-			'bootstrap-script',	get_template_directory_uri() . '/lib/vendor/bootstrap/js/bootstrap.min.js', array('jquery'), '3.3.4', true);
+			'bootstrap',
+			get_template_directory_uri() . '/lib/vendor/bootstrap/js/bootstrap.min.js',
+			array('jquery'),
+			'3.3.4',
+			true
+			);
 
 		// bootstrap css
 		wp_enqueue_style(
-			'bootstrap-style', get_stylesheet_directory_uri() . '/lib/vendor/bootstrap/css/bootstrap.min.css' );
+			'bootstrap',
+			get_stylesheet_directory_uri() . '/lib/vendor/bootstrap/css/bootstrap.min.css',
+			array(),
+			'1.0.0',
+			'all'
+			);
+
+		// Studio css
+		wp_enqueue_style(
+			'studiocss',
+			get_stylesheet_directory_uri() . '/lib/styles/main.css',
+			array(),
+			'1.0.0',
+			'all'
+			);
 
 		// fontawesome
 		wp_enqueue_style(
-			'fontawesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css' );
+			'fontawesome',
+			'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'
+			);
 
 		//slideshow for font-page js (24 jan 2017)
 		wp_enqueue_script(
@@ -100,4 +131,13 @@
 ==================================================== */
 	add_theme_support( 'post-thumbnails' );
 
-?>
+/* =================================================
+    Search Query: Search only on the Blog
+==================================================== */
+	function customsearchFilter($query) {
+		if ($query->is_search) {
+			$query->set('post_type', 'post');
+			}
+		return $query;
+		}
+	add_filter('pre_get_posts','customsearchFilter');
