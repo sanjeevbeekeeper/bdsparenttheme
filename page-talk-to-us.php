0f447960ba703eed08e20639f2eff8117bfbba36
Sanjeev Beekeeper PC office <sanjeevbeekeeper@gmail.com>
2017-03-09 10:50:21 +0530

    I lost track.

--- page-talk-to-us.php
+++ page-talk-to-us.php
@@ -15,7 +15,7 @@
                 </div>
 
                 <!-- talk to us form -->
-                <div class="col-xs-10 col-xs-offset-1 col-sm-12 col-sm-offset-0 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1">
+                <div class="col-xs-10 col-xs-offset-1 col-sm-12 col-sm-offset-0 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
                     <div class="talktous_form">
 
                         <!-- Loop start -->
