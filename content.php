0d3b45c8a1c2f02c4beb30557569c4986adfd4ac
Sanjeev Beekeeper PC office <sanjeevbeekeeper@gmail.com>
2017-03-27 15:28:45 +0530

    Updated with new design.

--- content.php
+++ content.php
@@ -5,22 +5,38 @@
 # search.php
 # single.php
 -->
-<article class="blog--article">
 
-    <a class="blog--article__title-date" href="<?php the_permalink(); ?>">
-        <h3>
-            <div class="truncate"><?php the_title(); ?></div>
-            <time> <?php the_time('d M Y'); ?></time>
-        </h3>
-    </a>
+<article class="blog--article">
+    <?php
+        // only for single page
+        if ( is_single() ) { ?>
+            <h3 class="blog_single">
+                <?php the_title(); ?>
+            </h3>
+        <?php
+            }
+        // for other pages (blog main page)
+        else {
+            ?>
+            <a class="blog--article__title-date" href="<?php the_permalink(); ?>">
+                <h3>
+                    <div class="truncate"><?php the_title(); ?></div>
+                    <time> <?php the_time('d M Y'); ?></time>
+                </h3>
+            </a>
+            <?php }
+        ?>
+
 
     <!-- content -->
     <?php
         if ( is_home() || is_archive() || is_search() ) {
             // the_excerpt();
             }
-        else {
-            the_content();
-            }
+        else { ?>
+            <div class="blog_para">
+                <?php the_content(); ?>
+            </div>
+            <?php }
         ?>
 </article>
