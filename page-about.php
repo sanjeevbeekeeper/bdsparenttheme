af3a94b616fe1d90fb8e3d1edafdfd84ccb25b26
Sanjeev Beekeeper PC office <sanjeevbeekeeper@gmail.com>
2017-03-27 15:31:09 +0530

    Cleaned up the code.

--- page-about.php
+++ page-about.php
@@ -9,7 +9,7 @@
     <div class="container">
         <!-- services -->
         <div class="row">
-            <div class="col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
+            <div class="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
                 <article class="about_container text-center">
                     <!--  about content -->
                     <?php the_content(); ?>
